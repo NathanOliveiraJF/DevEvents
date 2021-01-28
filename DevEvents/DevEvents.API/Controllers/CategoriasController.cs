53339191341f5ad1a62f193a439c6940fbac3a83
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2021-01-28 17:23:36 -0300

    Aula 02

---
+++ DevEvents/DevEvents.API/Controllers/CategoriasController.cs
@@ -0,0 +1,43 @@
+﻿using DevEvents.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Controllers
+{
+    [Route("api/categorias")]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterCategorias()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterCategoria(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Cadastrar(int id, [FromBody] Categoria categoria)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody] Categoria categoria)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevEvents/DevEvents.API/Controllers/UsuariosController.cs
@@ -0,0 +1,44 @@
+﻿using DevEvents.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Controllers
+{
+    [Route("api/usuarios")]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterUsuarios()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterUsuario(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Cadastrar(int id, [FromBody] Usuario usuario)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, [FromBody] Usuario usuario)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            return NoContent();
+        }
+
+    }
+}
